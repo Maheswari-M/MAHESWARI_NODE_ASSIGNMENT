0f8b9021f832ca75d0a39528f0984828dd368d1f
Maheswari-M <37100171+Maheswari-M@users.noreply.github.com>
2018-03-22 10:34:07 +0530

    Add files via upload

---
+++ checkout/app.js
@@ -0,0 +1,60 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var db = require("./db");
+var config = require("./config");
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 8090;
+var router = express.Router();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Accept"
+  );
+  next();
+});
+var enableCORS = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, *"
+  );
+
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(enableCORS);
+
+var checkout = require("./routes/Checkout");
+app.use("/checkout", checkout);
+
+app.use(function(req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(config.app.port, "172.16.110.38", () => {
+  console.log("REST API is runnning at " + config.app.port);
+});
